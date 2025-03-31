c11283379a91bd109b0eb7edabd33c1f7a340306
IDeal <cyon256@icloud.com>
2025-04-01 08:24:51 +0900

    trait object

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,71 @@
-use std::error::Error;
-use reqwest;
+use rand::{
+    self,
+    seq::SliceRandom,
+    Rng,
+};
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let url = "https://chatgpt.com/c/67eb0d59-c1fc-8001-a2ae-4085b0061cc5";
-    let mut response = reqwest::get(url)?;
-    let content = response.text()?;
-    print!("{}", content);
-    Ok(())
+
+#[derive(Debug)]
+struct Dwarf { }
+
+#[derive(Debug)]
+struct Elf { }
+
+#[derive(Debug)]
+struct Human { }
+
+#[derive(Debug)]
+enum Thing {
+    Sword,
+    Trinket,
+}
+
+trait Enchanter: std::fmt::Debug {
+    fn competency(&self) -> f64;
+
+    fn enchant(&self, thing: &mut Thing) {
+        let probability_of_success = self.competency();
+        let spell_is_successful = rand::thread_rng()
+            .gen_bool(probability_of_success);
+
+        print!("{:?} mutters incoherently. ", self);
+        if spell_is_successful {
+            println!("The {:?} glows brightly.", thing);
+        } else {
+            println!("The {:?} fizzes, \
+                then turns into a worthless trinket.", thing);
+            *thing = Thing::Trinket {};
+        }
+    }
+}
+
+impl Enchanter for Dwarf {
+    fn competency(&self) -> f64 {
+        0.5
+    }
+}
+
+impl Enchanter for Elf {
+    fn competency(&self) -> f64 {
+        0.95
+    }
+}
+
+impl Enchanter for Human {
+    fn competency(&self) -> f64 {
+        0.8
+    }
+}
+
+fn main() {
+    let mut it = Thing::Sword;
+
+    let d = Dwarf{};
+    let e = Elf {};
+    let h = Human{};
+
+    let party: Vec<&dyn Enchanter> = vec![&d, &h, &e];
+    let spellcaster = party.choose(&mut rand::thread_rng()).unwrap();
+
+    spellcaster.enchant(&mut it);
 }
