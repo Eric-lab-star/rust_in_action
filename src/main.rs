f8b5f0e5fb35c9b5d91fb5bd77059ba642ac2b90
IDeal <cyon256@icloud.com>
2025-04-04 05:59:48 +0900

    get ip address

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,74 @@
-use rand::{
-    self,
-    seq::SliceRandom,
-    Rng,
-};
+use std::net::{SocketAddr, UdpSocket};
+use std::time::Duration;
 
-
-#[derive(Debug)]
-struct Dwarf { }
-
-#[derive(Debug)]
-struct Elf { }
-
-#[derive(Debug)]
-struct Human { }
-
-#[derive(Debug)]
-enum Thing {
-    Sword,
-    Trinket,
-}
-
-trait Enchanter: std::fmt::Debug {
-    fn competency(&self) -> f64;
-
-    fn enchant(&self, thing: &mut Thing) {
-        let probability_of_success = self.competency();
-        let spell_is_successful = rand::thread_rng()
-            .gen_bool(probability_of_success);
-
-        print!("{:?} mutters incoherently. ", self);
-        if spell_is_successful {
-            println!("The {:?} glows brightly.", thing);
-        } else {
-            println!("The {:?} fizzes, \
-                then turns into a worthless trinket.", thing);
-            *thing = Thing::Trinket {};
-        }
-    }
-}
-
-impl Enchanter for Dwarf {
-    fn competency(&self) -> f64 {
-        0.5
-    }
-}
-
-impl Enchanter for Elf {
-    fn competency(&self) -> f64 {
-        0.95
-    }
-}
-
-impl Enchanter for Human {
-    fn competency(&self) -> f64 {
-        0.8
-    }
-}
+use clap::{App, Arg};
+use rand;
+use trust_dns::op::{ Message, MessageType, OpCode, Query};
+use trust_dns::rr::domain::Name;
+use trust_dns::rr::record_type::RecordType;
+use trust_dns::serialize::binary::*;
 
 fn main() {
-    let mut it = Thing::Sword;
+    let app = App::new("resolve")
+        .about("A simple to use DNS resolver")
+        .arg(Arg::with_name("dns-server").short("s").default_value("1.1.1.1"))
+        .arg(Arg::with_name("domain-name").required(true))
+        .get_matches();
 
-    let d = Dwarf{};
-    let e = Elf {};
-    let h = Human{};
+    let domain_name_raw =
+        app.value_of("domain-name").unwrap();
+    let domain_name = 
+        Name::from_ascii(&domain_name_raw).unwrap();
+    let dns_server_raw =
+        app.value_of("dns-server").unwrap();
+    let dns_server: SocketAddr = 
+        format!("{}:53", dns_server_raw)
+        .parse()
+        .expect("invalid address");
 
-    let party: Vec<&dyn Enchanter> = vec![&d, &h, &e];
-    let spellcaster = party.choose(&mut rand::thread_rng()).unwrap();
+    let mut request_as_bytes: Vec<u8> =
+        Vec::with_capacity(512);
+    let mut response_as_bytes: Vec<u8> =
+        vec![0;512];
 
-    spellcaster.enchant(&mut it);
+    let mut msg = Message::new();
+    msg
+        .set_id(rand::random::<u16>())
+        .set_message_type(MessageType::Query)
+        .add_query(
+            Query::query(domain_name, RecordType::A))
+        .set_op_code(OpCode::Query)
+        .set_recursion_desired(true);
+
+    let mut encoder = 
+        BinEncoder::new(&mut request_as_bytes);
+    msg.emit(&mut encoder).unwrap();
+
+    let localhost = UdpSocket::bind("0.0.0.0:0")
+        .expect("cannot bind to local socket");
+    let timeout = Duration::from_secs(3);
+    localhost.set_read_timeout(Some(timeout)).unwrap();
+    localhost.set_nonblocking(false).unwrap();
+
+    let _amt = localhost
+        .send_to(&request_as_bytes, dns_server)
+        .expect("socket misconfigured");
+
+    let (_amt, _remote) = localhost
+        .recv_from(&mut response_as_bytes)
+        .expect("timeout reached");
+
+    let dns_message = Message::from_vec(&response_as_bytes)
+        .expect("unsable to parse response");
+
+    for answer in dns_message.answers() {
+        if answer.record_type() == RecordType::A {
+            let resource = answer.rdata();
+            let ip = resource
+                .to_ip_addr()
+                .expect("invalid IP address received");
+            println!("{}",ip.to_string());
+        }
+
+    }
 }
